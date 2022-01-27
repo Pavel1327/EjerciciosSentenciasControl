51260484bf9b8e1da512e4e88e4fb3f8d0373a90
ASUS <ASUS@LAPTOP-FSORRA84>
2022-01-26 22:35:59 -0500

    Esta es la segunda version del proyecto de la asignatura de POO

---
+++ src/ec/edu/intsuperior/controlador/Pais.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.controlador;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pais {
+    private String codigoPais;
+    private String nombrePais;
+
+    public Pais() {
+    }
+
+    public Pais(String codigoPais, String nombrePais) {
+        this.codigoPais = codigoPais;
+        this.nombrePais = nombrePais;
+    }
+
+    public void setCodigoPais(String codigoPais) {
+        this.codigoPais = codigoPais;
+    }
+
+    public void setNombrePais(String nombrePais) {
+        this.nombrePais = nombrePais;
+    }
+
+    public String getCodigoPais() {
+        return codigoPais;
+    }
+
+    public String getNombrePais() {
+        return nombrePais;
+    }
+    
+    }
